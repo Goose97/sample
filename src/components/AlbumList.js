a50e12e4627f21c53143ed47d623ccc2f899b020
phamthucanh <annie.pham@manadr.com>
2018-07-10 21:51:09 +0700

    Commit o quan tra sua

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,17 +1,25 @@
 import React, { Component } from 'react';
 import { View, Text} from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 
 class AlbumList extends Component {
+  state = { album : [] };
+  
   componentWillMount() {
     axios.get('http://rallycoding.herokuapp.com/api/music_albums')
       .then(response => console.log(response));
   }
 
+  renderAlbums() {
+    return this.state.album.map(album => 
+      <AlbumDetail key={album.title} album={album} />;
+  }
+
   render() {
     return(
       <View>
-        <Text>1234</Text>
+        {this.renderAlbums()}
       </View>
     );
   }
